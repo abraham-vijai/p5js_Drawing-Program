988e13e937bb20f6b4325ccd47bef75a4ea4676a
Abraham Vijai <abrahamvijai03@gmail.com>
2024-09-30 20:04:46 -0400

    Created border

--- sketch.js
+++ sketch.js
@@ -4,8 +4,13 @@ function setup() {
 
 function draw() {
   // Create Toolbar Area
+  noStroke();
   fill('red');
   rectMode(CORNER);
   rect(0, 0, 150, windowHeight);
-
+  
+  // Create Border
+  stroke('black')
+  strokeWeight(10)
+  line(150, 0, 150, windowHeight)
 }
