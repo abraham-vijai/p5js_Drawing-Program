227f08d0206854984cfbc7855cb5d45e96d5e499
Abraham Vijai <abrahamvijai03@gmail.com>
2024-10-02 14:16:10 -0400

    Created shape class

---
+++ shapes.js
@@ -0,0 +1,38 @@
+class Shapes {
+    constructor() {
+
+    }
+
+    static drawRectangle(strokeColor, mode, weight, p1, p2, p3, p4, fillColor) {
+        fill(fillColor)
+        stroke(strokeColor);
+        rectMode(mode);
+        strokeWeight(weight);
+        rect(p1, p2, p3, p4);
+    }
+
+    static drawPoint(color, weight, x, y) {
+        stroke(color);
+        strokeWeight(weight);
+        point(x, y);
+    }
+
+    static drawLine(color, weight, x1, y1, x2, y2) {
+        stroke(color)
+        strokeWeight(weight)
+        line(x1, y1, x2, y2)
+    }
+
+    static drawImage(myImage, mode, p1, p2, p3, p4) {
+        imageMode(mode);
+        image(myImage, p1, p2, p3, p4);
+    }
+
+    static drawText(horizAlign, vertAlign, label, weight, fillColor, p1, p2, p3, p4) {
+        fill(fillColor);
+        strokeWeight(weight);
+        textAlign(horizAlign, vertAlign);
+        text(label, p1, p2);
+        noFill();
+    }
+}
